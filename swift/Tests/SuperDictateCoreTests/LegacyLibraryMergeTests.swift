76b8a621c6553594f0cb9a1ea7b1dcd9fb155fb8
IMONsergey <jabba.person@gmail.com>
2026-08-08 09:18:48 +0300

    Run legacy Library merge cases under production ProductStateTests filter

--- swift/Tests/SuperDictateCoreTests/LegacyLibraryMergeTests.swift
+++ swift/Tests/SuperDictateCoreTests/LegacyLibraryMergeTests.swift
@@ -2,7 +2,7 @@ import Foundation
 import XCTest
 @testable import SuperDictateCore
 
-final class LegacyLibraryMergeTests: XCTestCase {
+extension ProductStateTests {
     func testMergeAddsMissingLegacyRecordingAndEvidence() {
         let entries = [
             SuperDictateLegacyHistoryEntry(
